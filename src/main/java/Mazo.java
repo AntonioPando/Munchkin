7b1a1d78328403020eecaef72d3365104ce831b2
Antonio Pando <antoniogpando@gmail.com>
2026-02-21 21:03:43 +0100

    Clase Mazo añadida

--- src/main/java/Mazo.java
+++ src/main/java/Mazo.java
@@ -5,8 +5,40 @@
 
 /**
  *
- * @author bigpa
+ * @author AntonioPando
  */
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+
 public class Mazo {
+    
+    protected ArrayList<Carta> cartas;
+
+    public Mazo() {
+        this.cartas = new ArrayList<>();
+    }
+    
+    public void Mezclar(){
+        Collections.shuffle(cartas);
+    }
+    
+    public Carta robar(){
+        if (cartas.isEmpty()){
+            return null;
+        }
+        return cartas.remove(0);
+    }
+    
+    public boolean estaVacio(){
+        return cartas.isEmpty();
+    }
+    
+    public int size(){
+        return cartas.size();
+    }
+    
+    
     
 }
